0cf61a494982cbe8e4869f8136949754ee6f6b13
varnesh215 <varnesh21@gmail.com>
2021-02-25 13:26:35 +0530

    updated code

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -6,6 +6,8 @@ public class Sample {
 		System.out.println("test2");
 		System.out.println("test3");
 		System.out.println("test4");
+		System.out.println("test5");
+		System.out.println("test6");
 		
 	}
 
