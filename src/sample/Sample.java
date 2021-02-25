4dbd5a14cca7acebfbe23e5fb4f599e18e0ac490
varnesh215 <varnesh21@gmail.com>
2021-02-25 12:31:02 +0530

    new

---
+++ src/sample/Sample.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
+
+}
